cfd9f805838bbcb11cf620b6a2ca67f69cc8744c
Octavyan Putra Ramadhan <octavyan4@gmail.com>
2023-12-15 21:54:17 +0700

    login page

--- src/components/beranda/item/FormPendaftaran.vue
+++ src/components/beranda/item/FormPendaftaran.vue
@@ -263,7 +263,7 @@
       <div v-for="cedera in cederas" :key="cedera.id">
         <q-item tag="label" v-ripple>
           <img
-            :src="'http://localhost:8000/storage/cederas/' + cedera.image"
+            :src="'http:///storage/cederas/' + cedera.image"
             alt="Cedera Image"
             width="80"
           />
--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -1,77 +1,75 @@
 <template>
-  <q-page
-    class="window-height window-width row justify-center items-center"
-    style="background: linear-gradient(#0079ca, #ffffff)"
-  >
-    <div class="column q-pa-lg">
-      <div class="row">
-        <q-card square class="shadow-24" style="width: 400px; height: 540px">
-          <q-card-section class="bg-deep-purple-7 q-pa-sm text-center">
-            <h4 class="text-h5 text-white q-my-md">Login dulu cuy</h4>
-          </q-card-section>
+  <q-page class="row justify-center items-center">
+    <div class="col-10">
+      <div
+        class="text-h5 text-primary text-bold q-mb-lg"
+        style="font-size: 30px; font-family: Poppins"
+      >
+        Log In
+      </div>
+      <q-form>
+        <!-- Email -->
+        <q-input
+          outlined
+          dense
+          v-model="email"
+          type="email"
+          lazy-rules
+          :rules="emailRules"
+          label="Email"
+          style="border-radius: 20px; margin-bottom: 10px"
+        >
+          <template v-slot:prepend>
+            <q-icon name="email" color="dark" />
+          </template>
+        </q-input>
 
-          <q-card-section>
-            <q-form class="q-px-sm q-pt-xl">
-              <!-- Email -->
-              <q-input
-                square
-                clearable
-                v-model="email"
-                type="email"
-                lazy-rules
-                :rules="emailRules"
-                label="Email"
-              >
-                <template v-slot:prepend>
-                  <q-icon name="email" />
-                </template>
-              </q-input>
-
-              <!-- Password -->
-              <q-input
-                square
-                clearable
-                v-model="password"
-                :type="passwordFieldType"
-                lazy-rules
-                :rules="passwordRules"
-                label="Password"
-              >
-                <template v-slot:prepend>
-                  <q-icon name="lock" />
-                </template>
-                <template v-slot:append>
-                  <q-icon
-                    :name="visibilityIcon"
-                    @click="switchVisibility"
-                    class="cursor-pointer"
-                  />
-                </template>
-              </q-input>
-            </q-form>
-          </q-card-section>
-
-          <q-card-actions class="q-px-lg">
-            <q-btn
-              unelevated
-              size="lg"
-              color="secondary"
-              :disable="loading"
-              @click="submit"
-              class="full-width text-white"
-              label="Login"
+        <!-- Password -->
+        <q-input
+          outlined
+          dense
+          v-model="password"
+          :type="passwordFieldType"
+          lazy-rules
+          :rules="passwordRules"
+          label="Password"
+        >
+          <template v-slot:prepend>
+            <q-icon name="lock" color="dark" />
+          </template>
+          <template v-slot:append>
+            <q-icon
+              :name="visibilityIcon"
+              @click="switchVisibility"
+              class="cursor-pointer"
             />
-          </q-card-actions>
+          </template>
+        </q-input>
+      </q-form>
 
-          <q-btn
-            to="/register"
-            flat
-            style="color: #003f88"
-            label="Tidak mempunyai akun?"
-          >
-            <q-tooltip>Register</q-tooltip>
-          </q-btn>
-        </q-card>
+      <div class="text-center text-grey" style="margin-bottom: 150px">
+        forgot a password
+      </div>
+
+      <q-btn
+        unelevated
+        rounded
+        size="lg"
+        color="primary"
+        :disable="loading"
+        @click="submit"
+        class="full-width text-white"
+        label="Login"
+      />
+
+      <div class="text-center q-mt-md">
+        don't have an account?
+        <router-link
+          to="/register"
+          class="text-warning"
+          style="text-decoration: none"
+          >sign up</router-link
+        >
       </div>
     </div>
   </q-page>
@@ -119,7 +117,7 @@ const submit = async () => {
     const res = await authStore.login(email.value, password.value);
 
     if (res.data.status === "Success") {
-      localStorage.setItem("token", res.data.data.token)
+      localStorage.setItem("token", res.data.data.token);
       router.push({ name: "beranda" });
       window.location.reload();
 
